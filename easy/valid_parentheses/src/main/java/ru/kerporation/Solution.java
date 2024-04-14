50a96eb70a0fceb2e002572a22abe4a1a9d7f4c1
kkozlovsky <kkozlovsky@yandex.ru>
2024-04-14 17:45:37 +0300

    Valid Parentheses

---
+++ easy/valid_parentheses/src/main/java/ru/kerporation/Solution.java
@@ -0,0 +1,21 @@
+package ru.kerporation;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        final Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
