f9f5938c503d344884b30ed034c8df4bd8e78667
kkozlovsky <kkozlovsky@yandex.ru>
2024-04-23 11:03:52 +0300

    best_time_to_buy_and_sell_stock

---
+++ easy/best_time_to_buy_and_sell_stock/src/test/java/ru/kerporation/SolutionTest.java
@@ -0,0 +1,16 @@
+package ru.kerporation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @Test
+    void maxProfit() {
+        final Solution solution = new Solution();
+        assertEquals(5, solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        assertEquals(4, solution.maxProfit(new int[]{1, 2, 3, 4, 5}));
+        assertEquals(0, solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
