50b3dc71f1d9c452cba5caf0901805c7617b5db9
kkozlovsky <kkozlovsky@yandex.ru>
2024-06-25 21:40:52 +0300

    group_anagrams1

--- medium/group_anagrams/src/main/java/ru/kerporation/Solution.java
+++ medium/group_anagrams/src/main/java/ru/kerporation/Solution.java
@@ -1,9 +1,6 @@
 package ru.kerporation;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(final String[] strs) {
@@ -28,6 +25,25 @@ class Solution {
         }
 
         // Collect all groups of anagrams
+        return new ArrayList<>(map.values());
+    }
+
+
+    public List<List<String>> groupAnagrams1(final String[] strs) {
+        final Map<String, List<String>> map = new HashMap<>();
+
+        for (final String word : strs) {
+            final char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            map.get(sortedWord).add(word);
+        }
+
         return new ArrayList<>(map.values());
     }
 }
