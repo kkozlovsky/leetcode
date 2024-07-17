965301082d8a62620acc7c7db19095f93432b1c9
kkozlovsky <kkozlovsky@yandex.ru>
2024-07-17 22:33:13 +0300

    sort_colors

---
+++ medium/sort_colors/src/main/java/ru/kerporation/Solution.java
@@ -0,0 +1,28 @@
+package ru.kerporation;
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int zeros = 0;
+        int ones = 0;
+        final int n = nums.length;
+        for (int num : nums) {
+            if (num == 0) {
+                zeros++;
+            } else if (num == 1) {
+                ones++;
+            }
+        }
+
+        for (int i = 0; i < zeros; ++i) {
+            nums[i] = 0;
+        }
+
+        for (int i = zeros; i < zeros + ones; ++i) {
+            nums[i] = 1;
+        }
+
+        for (int i = zeros + ones; i < n; ++i) {
+            nums[i] = 2;
+        }
+    }
+}
